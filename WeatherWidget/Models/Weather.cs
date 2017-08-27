716ef45d5a765439e6ec6b2cb3eb49ab80e9e769
michael_townsend <michael.h.townsend@gmail.com>
2017-08-27 13:04:06 -0400

    Home/Index started, Weather class added -- building DB off this

---
+++ WeatherWidget/Models/Weather.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WeatherWidget.Models
+{
+    public class Weather
+    {
+        public int Id { get; set; }
+        public double PrecipitationChance { get; set; }
+        public int Temperature { get; set; }
+        public string Description { get; set; }
+
+        public Weather()
+        {
+
+        }
+
+        public Weather(double PrecipitationChange, int Temperature, string Description)
+        {
+            this.PrecipitationChance = PrecipitationChance;
+            this.Temperature = Temperature;
+            this.Description = Description;
+        }
+    }
+}
