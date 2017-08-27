5b80641828c1526a9b2e2bd537e00f901acded07
michael_townsend <michael.h.townsend@gmail.com>
2017-08-27 16:23:38 -0400

    Weather model scaffolding added

---
+++ WeatherWidget/Models/WeathersController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WeatherWidget.Models
+{
+    public class WeathersController : Controller
+    {
+        private WeatherDbContext db = new WeatherDbContext();
+
+        // GET: Weathers
+        public ActionResult Index()
+        {
+            return View(db.Weather.ToList());
+        }
+
+        // GET: Weathers/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Weather weather = db.Weather.Find(id);
+            if (weather == null)
+            {
+                return HttpNotFound();
+            }
+            return View(weather);
+        }
+
+        // GET: Weathers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Weathers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,ZipCode,PrecipitationChance,Temperature,Description")] Weather weather)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Weather.Add(weather);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(weather);
+        }
+
+        // GET: Weathers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Weather weather = db.Weather.Find(id);
+            if (weather == null)
+            {
+                return HttpNotFound();
+            }
+            return View(weather);
+        }
+
+        // POST: Weathers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,ZipCode,PrecipitationChance,Temperature,Description")] Weather weather)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(weather).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(weather);
+        }
+
+        // GET: Weathers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Weather weather = db.Weather.Find(id);
+            if (weather == null)
+            {
+                return HttpNotFound();
+            }
+            return View(weather);
+        }
+
+        // POST: Weathers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Weather weather = db.Weather.Find(id);
+            db.Weather.Remove(weather);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
